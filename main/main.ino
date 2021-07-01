366824cc3a07c7d47fe495c01c32f36784512048
Amin <dadgaramin96@gmail.com>
2021-07-01 21:11:33 +0430

    OTA functionality implemented!

--- main/main.ino
+++ main/main.ino
@@ -12,6 +12,9 @@
 #include "credentials.c"
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <string.h>
 
 
@@ -28,6 +31,13 @@ void checkDisconnectedTime(unsigned int currentTime, unsigned int previousMillis
 
 
 void setup() {
+
+
+  Serial.begin(115200);
+  delay(100);
+  Serial.println();
+  Serial.println("Booting");
+  
   // since we just have relay's here 
   // we make all them as output
   pinMode(relay1,OUTPUT);
@@ -50,7 +60,7 @@ void setup() {
   digitalWrite(relay8,HIGH);
   
   
-  Serial.begin(115200);
+
 
 
   // get credentials from c function
@@ -83,6 +93,55 @@ void setup() {
   Serial.print("Connected to WiFi :");
   Serial.println(WiFi.SSID());
   digitalWrite(LED,LOW);
+
+
+  // Codes for Arduino Over the Air programing (OTA)
+  
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 
 
   // Setup mqtt server config
@@ -269,6 +328,7 @@ void checkDisconnectedTime(unsigned int currentTime, unsigned int previousMillis
 }
 
 void loop() {
+  ArduinoOTA.handle(); // Handle OTA connection
   client.loop();
 
   // check mqtt connection here
